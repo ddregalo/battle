04c657a2234167e5e21353b01dabd6fd147efe31
danielcampos <ddregalo@gmail.com>
2018-03-07 13:42:16 +0000

    Refactor tests - Add web_helpers for rspec set-up, update spec_helper

--- spec/features/health_points_spec.rb
+++ spec/features/health_points_spec.rb
@@ -1,6 +1,6 @@
 feature 'Show Player Health Points' do
   scenario 'display player health' do
-    visit('/play')
+    sign_in_and_play
     expect(page).to have_content "Rothko: 60 Health"
   end
 end
--- spec/features/player_names_spec.rb
+++ spec/features/player_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Get Player 1 & Player 2 Names' do
   scenario 'submitting names in form' do
-    visit('/')
-    fill_in :player_1_name, with: 'Samo'
-    fill_in :player_2_name, with: 'Rothko'
-    click_button 'Start Match'
+    sign_in_and_play
     expect(page).to have_content "Samo vs. Rothko"
   end
 end

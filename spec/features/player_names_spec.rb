14943c2343864952497d8e224e8748134b430969
danielcampos <ddregalo@gmail.com>
2018-03-07 12:00:10 +0000

    Use sessions to print player names to play view - test passed

--- spec/features/player_names_spec.rb
+++ spec/features/player_names_spec.rb
@@ -4,6 +4,6 @@ feature 'Get Player 1 & Player 2 Names' do
     fill_in :player_1_name, with: 'Samo'
     fill_in :player_2_name, with: 'Rothko'
     click_button 'Start Match'
-    expect(page).to have_content 'Samo vs. Rothko'
+    expect(page).to have_content "Samo vs. Rothko"
   end
 end
